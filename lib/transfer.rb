d8850c1e1e2c840cd80b8bbf53d4aaacc31efe32
Joellehelm <joellenhelm@gmail.com>
2019-09-19 14:32:08 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
+require_relative "bank_account"
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def self.valid?
+    @sender.valid? == true && @receiver.valid? == true
+  end
+  
+  
+
 end
